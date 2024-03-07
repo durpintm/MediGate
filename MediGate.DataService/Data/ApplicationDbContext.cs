93c0bce69715806dcfa1ff58969214c2b7caf62b
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-06 20:00:28 -0500

    Adding project references

--- MediGate.DataService/Data/ApplicationDbContext.cs
+++ MediGate.DataService/Data/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MediGate.Entities.DbSet;
+
 namespace MediGate.DataService.Data
 {
-  public class ApplicationDbContext: IdentityDbContext{
+  public class ApplicationDbContext : IdentityDbContext
+  {
+
+    public virtual DbSet<User> Users { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-      
+
     }
   }
 }
