cf1b2c24069eaae8aafbfd96611a53e60ce41ca7
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-07 18:04:56 -0500

    Configuring JWT Auth

---
+++ MediGate.Api/Configuration/Models/JwtConfiguration.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediGate.Api.Configuration.Models
+{
+    public class JwtConfiguration
+    {
+        public string Secret { get; set; }
+    }
+}
--- MediGate.Api/Program.cs
+++ MediGate.Api/Program.cs
@@ -1,9 +1,15 @@
+using System.Text;
+using MediGate.Api.Configuration.Models;
 using MediGate.DataService.Data;
 using MediGate.DataService.IConfiguration;
 using MediGate.DataService.IRepository;
 using MediGate.DataService.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +22,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+// Update the Jwt Config from the settings
+builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetSection("JwtConfig"));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddApiVersioning(options =>
@@ -28,6 +36,34 @@ builder.Services.AddApiVersioning(options =>
     options.DefaultApiVersion = ApiVersion.Default;
 });
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+{
+    // Getting the secret from the config
+    var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false, // ToDo Update
+        ValidateAudience = false, // ToDo Update
+        RequireExpirationTime = false, // ToDo Update
+        ValidateLifetime = true
+    };
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -38,7 +74,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
 
