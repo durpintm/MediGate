36dbd535aba47333427fa7513a3ef46149ba09f4
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-09 00:21:57 -0500

    User DTO update

--- MediGate.Entities/DbSet/User.cs
+++ MediGate.Entities/DbSet/User.cs
@@ -11,5 +11,8 @@ namespace MediGate.Entities.DbSet
     public string Phone { get; set; }
     public DateTime DateOfBirth { get; set; }
     public string Country { get; set; }
+    public string Address { get; set; }
+    public string MobileNumber { get; set; }
+    public string Sex { get; set; }
   }
 }
